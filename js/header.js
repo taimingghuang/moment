6f658bf3128afd7fef6bf6337da5b287b0df189c
taimingghuang <taimingghuang@gmail.com>
2025-10-30 02:58:07 +0800

    backup251030

--- js/header.js
+++ js/header.js
@@ -1,11 +1,11 @@
 function initHeader() {
   console.log('initHeader 被呼叫了');
   
-  // 漢堡選單
+  // hamburger
   const menu = document.querySelector('.menu');
-  const hamburgerMenu = document.querySelector('.menu ul'); // 漢堡選單的 ul
+  const hamburgerMenu = document.querySelector('.menu ul'); // 漢堡選單's ul
   
-  // 購物車
+  // cart
   const cartIcon = document.querySelector('.cart-icon');
   const cartPanel = document.querySelector('.cart-panel-wrapper');
   const closeBtn = document.querySelector('.cart-close-btn');
@@ -69,3 +69,77 @@ function initHeader() {
     }
   });
 }
+
+
+
+// jQuery寫法:
+// function initHeader {
+//   console.log(`被呼叫了`)
+
+//   // 設定變數
+//   let $menu = $('.menu')
+//   let $hamburgerMenu = $('.menu ul')
+
+//   let $cartIcon = $('.cart-icon')
+//   let $cartPanel = $('.cart-panel-wrapper') 
+//   let $closeBtn = $('.cart-close-btn')
+
+//   // 先檢查
+//   if ($cartIcon.lenth === 0 || $cartPanel.length === 0 || $closeBtn === 0 ){
+//     return;
+//   }
+
+//   // 事件處理 
+//   // 漢堡選單點擊
+//   // jQuery 的 .on('click', ...) 和 .click(...) 已經自動處理了 this 的問題
+//   $menu.on('click', function(e) {
+//     e.stopPropagation();
+//     $(this).toggleClass('active'); // 使用 $(this) 操作當前被點擊的元素
+//     // 關閉購物車
+//     $cartPanel.removeClass('active');
+//   });
+
+//   // 購物車圖示點擊
+//   $cartIcon.on('click', function(e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     $cartPanel.toggleClass('active');
+//     // 關閉漢堡選單
+//     $menu.removeClass('active');
+//     console.log('購物車被點擊');
+//   });
+
+//   // 關閉按鈕
+//   $closeBtn.on('click', function(e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     $cartPanel.removeClass('active');
+//   });
+
+//   // 點擊購物車面板內部時不關閉
+//   $cartPanel.on('click', function(e) {
+//     e.stopPropagation();
+//   });
+
+//   // 點擊外部關閉購物車和漢堡選單 (document 點擊)
+//   $(document).on('click', function(e) {
+//     // .is() 檢查 e.target 是否是 $cartIcon 或 $cartPanel
+//     // .has() 檢查 e.target 是否為 $cartIcon 或 $cartPanel 的子元素
+//     if (!$(e.target).closest($cartIcon).length && !$(e.target).closest($cartPanel).length) {
+//        $cartPanel.removeClass('active');
+//     }
+    
+//     // 檢查點擊是否在 $menu 內部
+//     if (!$menu.has(e.target).length && !$menu.is(e.target)) {
+//       $menu.removeClass('active');
+//     }
+//   });
+
+//   // ESC 鍵關閉 (document 監聽 keydown)
+//   $(document).on('keydown', function(e) {
+//     if (e.key === 'Escape') {
+//       $cartPanel.removeClass('active');
+//       $menu.removeClass('active');
+//     }
+//   });
+// }
